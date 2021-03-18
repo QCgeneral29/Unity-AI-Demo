bfd4baa7f8780ad66a96bd74ad079ae31193196f
Landen Love <landenoooo@gmail.com>
2021-03-18 16:46:19 -0500

    Initial Commit

---
+++ Assets/AIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    
+    public Transform destination;
+
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(destination != null)
+        {
+            UpdateDestintation();
+        }
+    }
+
+    private void UpdateDestintation()
+    {
+        Vector3 targetVector = destination.transform.position;
+        navMeshAgent.SetDestination(targetVector);
+    }
+}
---
+++ Assets/MoveWaypoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWaypoint : MonoBehaviour
+{
+    private float xinput, zinput = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            zinput++;
+        }
+        if (Input.GetKey("s"))
+        {
+            zinput--;
+        }
+        if (Input.GetKey("a"))
+        {
+            xinput--;
+        }
+        if (Input.GetKey("d"))
+        {
+            xinput++;
+        }
+
+        zinput *= 0.05f;
+        xinput *= 0.05f;
+
+        Vector3 currentPos = this.transform.position;
+        currentPos.x -= zinput;
+        currentPos.z += xinput;
+
+        this.transform.position = currentPos;
+        xinput = 0;
+        zinput = 0;
+    }
+}
